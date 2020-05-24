f0b85dda2cfb8352d363186b60f8fcfd18c14b8c
anaalmeida1708 <anacarolinama@gmail.com>
2020-05-24 15:08:34 -0300

    Aula 13 - JavaScript

---
+++ JavaScript - Curso em vídeo/Aula 13 - Repetições parte 1/ambiente.js
@@ -0,0 +1,15 @@
+//Estrutura while
+console.log('Estrutura while')
+var c = 1
+while(c <= 10){
+    console.log(c)
+    c++
+}
+
+//Estrutura Do ... While
+console.log('Estrutura Do...While')
+var i = 1
+do{
+    console.log(i)
+    i++
+}while(i <= 10)

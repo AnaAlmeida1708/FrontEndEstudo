01b76c5acb0bc8962138ff1bdff58384d77f8511
anaalmeida1708 <anacarolinama@gmail.com>
2020-05-24 15:15:58 -0300

    Aula 14 Repetições parte 2 - JavaScript

---
+++ JavaScript - Curso em vídeo/Aula 14 - Repetições parte 2/ambiente.js
@@ -0,0 +1,11 @@
+//Estrutura de repetição FOR
+for(var c = 1; c <= 5; c++){
+    console.log(`C vale ${c}`)
+}
+
+//Estrutura While
+var i = 10
+while(i >= 1 ){
+    console.log(`I vale ${i}`)
+    i--
+}

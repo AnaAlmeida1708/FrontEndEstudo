be7d557e74d9480890bc01a479fa6988de4ed124
anaalmeida1708 <anacarolinama@gmail.com>
2020-06-30 10:17:09 -0300

    Criado restante do CRUD de tarefas e atualizar status - Angular

--- Angular/Formação Angular - Udemy/projetos/gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
+++ Angular/Formação Angular - Udemy/projetos/gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
@@ -23,5 +23,35 @@ export class TarefaService {
     localStorage['tarefas'] = JSON.stringify(tarefas); //salvamos a lista, convertendo ela para String que é o formato que o localStorage trabalha
   }
 
-  
+  buscarPorId(id: number): Tarefa {
+    const tarefas = this.listarTodos();
+    return tarefas.find(tarefa => tarefa.id === id);//itera sobre a lista de tarefas buscando o id informado
+  }
+
+  atualizar(tarefa: Tarefa): void {
+    const tarefas:Tarefa[] =  this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {//obj:tarefa, objs:lista de tarefas
+      if(tarefa.id === obj.id) {
+        objs[index] = tarefa;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  remover(id: number): void {
+    let tarefas: Tarefa[] = this.listarTodos();
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id);//busca todas as tarefas menos a que o id foi informado, é uma outra forma de iterar na lista
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  alterarStatus(id: number): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if(id === obj.id) {
+        objs[index].concluida = !obj.concluida;//inverte o  valor booleano
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
 }

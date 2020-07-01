4429658b5fe1b8bea5eaa5ae4cf11639551786d3
anaalmeida1708 <anacarolinama@gmail.com>
2020-07-01 13:06:58 -0300

    Registrando as rotas de listagem de tarefas - Angular

--- Angular/Formação Angular - Udemy/projetos/gerenciador-de-tarefas/src/app/app-routing.module.ts
+++ Angular/Formação Angular - Udemy/projetos/gerenciador-de-tarefas/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TarefaRoutes } from './tarefas';
+
 export const routes: Routes = [
+    
+    //aqui dizemos que se a URL estiver "vazia" pra ele redirecionar pra essa url determinada
+    {
+        path:'',
+        redirectTo: '/tarefas/listar',
+        pathMatch: 'full'
+    },
+    ...TarefaRoutes //concatena a url principal com a da rota
 ];
 
 @NgModule({

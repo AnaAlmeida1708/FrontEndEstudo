adcdcf1fc9fd4e3b11481efde63b998aedaadbf7
anaalmeida1708 <anacarolinama@gmail.com>
2020-07-02 07:35:08 -0300

    Implementando o cadastro de tarefas parte 1 - Angular

--- Angular/Formação Angular - Udemy/projetos/gerenciador-de-tarefas/src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
+++ Angular/Formação Angular - Udemy/projetos/gerenciador-de-tarefas/src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { TarefaService, Tarefa } from '../shared';
 
 @Component({
   selector: 'app-cadastrar-tarefa',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarTarefaComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTarefa', { static: true }) formTarefa: NgForm;//injeta uma versão do formulário preenchido
+  tarefa:Tarefa;//para gerar o valor da view dentro do nosso component
 
-  ngOnInit(): void {
+  constructor(
+    private tarefaService: TarefaService,
+    private router: Router) { }//usado depois para redirecionar a página quando a tarefa é cadastrada
+
+  ngOnInit() {
+    this.tarefa = new Tarefa();//inicialização da tarefa para associar com a interface
   }
+
+  cadastrar(): void {
+    if(this.formTarefa.form.valid) { //para só permitir que uma tarefa seja associada se estiver dentro do padrão definido, validado
+      this.tarefaService.cadastrar(this.tarefa);
+      this.router.navigate(["/tarefas"]); //metodo navigate para redirecionar
+    }
+  }
+
 
 }

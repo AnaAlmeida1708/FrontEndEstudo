027ccc8e971a969e14b6642b8b9b4ac04a8325f8
anaalmeida1708 <anacarolinama@gmail.com>
2020-07-03 08:15:42 -0300

    Comentando trechos de teste unitário - Angular

--- Angular/Formação Angular - Udemy/projetos/gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.spec.ts
+++ Angular/Formação Angular - Udemy/projetos/gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.spec.ts
@@ -10,7 +10,7 @@ describe('TarefaService', () => {
     service = TestBed.inject(TarefaService);
   });
 
-  it('should be created', () => {
+/*   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
+  }); */
 });

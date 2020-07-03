cc32a938127c442b63e564bb7d43199ecba23329
anaalmeida1708 <anacarolinama@gmail.com>
2020-07-03 08:50:28 -0300

    Criado o componente Jogo da velha - Angular

--- Angular/Formação Angular - Udemy/projetos/jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ Angular/Formação Angular - Udemy/projetos/jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JogoDaVelhaComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [//necessário pois não vamos trabalhar com rotas e vamos renderizar as tags do compnente direto no app component
+    JogoDaVelhaComponent
   ]
 })
 export class JogoDaVelhaModule { }

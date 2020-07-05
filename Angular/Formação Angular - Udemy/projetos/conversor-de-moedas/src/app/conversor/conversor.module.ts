128d609e4b8334217105b82462e2d49a7176ad3f
anaalmeida1708 <anacarolinama@gmail.com>
2020-07-05 16:18:22 -0300

    Criado serviço de gerenciamento de moedas - Angular

--- Angular/Formação Angular - Udemy/projetos/conversor-de-moedas/src/app/conversor/conversor.module.ts
+++ Angular/Formação Angular - Udemy/projetos/conversor-de-moedas/src/app/conversor/conversor.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ConversorComponent } from './components';
+import { MoedaService } from './services';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { ConversorComponent } from './components';
   ],
   exports: [
     ConversorComponent
-  ]
+  ],
+  providers: [MoedaService]
 })
 export class ConversorModule { }

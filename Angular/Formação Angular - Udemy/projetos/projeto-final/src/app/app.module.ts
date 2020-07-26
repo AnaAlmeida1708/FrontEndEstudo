25e963b921398d996c0294b0c4374c7cad970f6f
anaalmeida1708 <anacarolinama@gmail.com>
2020-07-26 18:50:34 -0300

    Configurado as rotas de navegação no componente principal - Angular

--- Angular/Formação Angular - Udemy/projetos/projeto-final/src/app/app.module.ts
+++ Angular/Formação Angular - Udemy/projetos/projeto-final/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule//este arquivo precisa ser sempre o último dos imports
   ],
   providers: [],
   bootstrap: [AppComponent]

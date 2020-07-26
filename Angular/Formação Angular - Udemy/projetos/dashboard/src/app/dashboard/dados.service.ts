98d0ae16018139d3578229df8aa2366dc73bb4e6
anaalmeida1708 <anacarolinama@gmail.com>
2020-07-26 10:35:30 -0300

    Implementado o Serviço de dados do Dashboard - Angular

--- Angular/Formação Angular - Udemy/projetos/dashboard/src/app/dashboard/dados.service.ts
+++ Angular/Formação Angular - Udemy/projetos/dashboard/src/app/dashboard/dados.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable, observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class DadosService {
 
+  readonly dados = [
+    ['Janeiro', 33],
+    ['Fevereiro', 68],
+    ['Março', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27]
+  ];
+
   constructor() { }
+
+  obterDados(): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.dados);//next comando para apresentar para todos que estão na escuta
+      observable.complete();//após a finalização informará que terminou e não precisa mais ficar na escuta
+    });
+    
+  }
 }
--- Angular/Formação Angular - Udemy/projetos/dashboard/src/app/dashboard/dashboard.module.ts
+++ Angular/Formação Angular - Udemy/projetos/dashboard/src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { DashboardComponent } from './dashboard.component';
 import { DadosService } from './dados.service';
 
